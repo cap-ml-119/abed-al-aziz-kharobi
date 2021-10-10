3f1ad81fbd8f26513e509475deaba3b984bdcabc
abedalaziz-kharobi <68472991+abedalaziz-kharobi@users.noreply.github.com>
2021-10-10 23:51:45 +0300

    Update route.py

--- route.py
+++ route.py
@@ -2,18 +2,23 @@ from flask import Flask
 import uuid
 app = Flask(__name__)
 
-# local date base : empty array we are added the data to it 
+# local date base : empty array we are added the data to it
 todo = []
 
-#add endpoint
+'#add endpoint'
+
+
 @app.route('/post/<string:name>/<string:status>', methods=['POST'])
-def add(name , status):
-    todo.append({"name" :name , "uid" : uuid.uuid4().int , "status" : status})
+def add(name, status):
+    todo.append({"name": name, "uid": uuid.uuid4().int, "status": status})
     return "added"
 
-#update endpoint
+
+'#update endpoint'
+
+
 @app.route('/update/<int:id>/<string:name>/<string:status>', methods=['PUT'])
-def update(id , name , status):
+def update(id, name, status):
     for i in todo:
         if(i["uid"] == id):
             i["name"] = name
@@ -21,18 +26,24 @@ def update(id , name , status):
     return "is update"
 
 # update the status by id
+
+
 @app.route('/update/<int:id>/<string:status>', methods=['PUT'])
-def update_status(id , status):
+def update_status(id, status):
     for i in todo:
         if(i["uid"] == id):
             i["status"] = status
 
 
-#get endpoint to test review my data
+'#get endpoint to test review my data'
+
+
 @app.route('/get', methods=['GET'])
 def re():
     return str(todo)
 
-#run the flask
+
+'#run the flask'
+
 if __name__ == "__main__":
     app.run(debug=True)
